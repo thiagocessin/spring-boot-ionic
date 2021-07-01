352815afcfb9ee9ba734dc291c675edc68044691
thiagocessin <thiagoacessin@gmail.com>
2021-07-01 16:26:07 -0300

    Email HTML

--- src/main/java/com/demo/services/AbstractEmailService.java
+++ src/main/java/com/demo/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.demo.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.demo.domain.Pedido;
 
@@ -11,6 +19,12 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Value("${default.sender}")
 	private String sender;
+	
+	@Autowired
+	private TemplateEngine template;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	//template method
 	@Override
@@ -29,6 +43,39 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		
 		return sm;
+	}
+	
+	//retornna o html string
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		//resources/templates -> padrão do thymeleaf
+		return template.process("email/confirmacaoPedido", context);
+	}
+	
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		MimeMessage mm;
+		try {
+			mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		
+		MimeMessage mm = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mm,true);
+		
+		mmh.setTo("thiagoacessin@gmail.com");//trocar para email do cliente
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido Confirmado - código "+ obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj),true);
+		
+		return mm;
 	}
 
 }
