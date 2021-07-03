f52a1d1dab5a2ae94b7ac73352742ae20bc4f5e6
thiagocessin <thiagoacessin@gmail.com>
2021-07-03 16:40:42 -0300

    Resize e crop da imagem de perfil

--- src/main/java/com/demo/services/ImageService.java
+++ src/main/java/com/demo/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -57,6 +58,20 @@ public class ImageService {
 			throw new FileException("Erro ao ler o arquivo");
 		}
 
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImage) {
+
+		int min = (sourceImage.getHeight() <= sourceImage.getWidth()) ? sourceImage.getHeight()
+				: sourceImage.getWidth();
+
+		return Scalr.crop(sourceImage, (sourceImage.getWidth() / 2) - (min / 2),
+				(sourceImage.getHeight() / 2) - (min / 2), min, min);
+	}
+
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 
 }
